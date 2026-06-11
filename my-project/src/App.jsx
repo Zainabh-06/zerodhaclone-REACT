17dcf579de4330393934ee708074e48fdbe9ca1c
Zainabh-06 <zainabh197006@gmail.com>
2026-06-11 16:41:46 +0530

    Updated Zerodha clone homepage

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,28 +1,24 @@
-import './App.css'
 
+import './App.css'
+import Header from './Header'
+import Products from './Products.jsx'
 function App() {
   
-
+ 
   return (
     <>
-    <section style={{textAlign:"center"}}>
-      <header style={{display:"flex", justifyContent:"space-evenly"}}>
-      <img src="https://zerodha.com/static/images/logo.svg" height={20} width={70}  alt="logo"/>
-
-      <nav style={{display:"flex", gap:"20px", alignItems:"center"}}>
-        <a href="">Signup</a>
-        <a href="">About</a>
-        <a href="">Pricing</a>
-        <a href="">Products</a>
-        <a href="">Support</a>
-      </nav>
-      </header>
-   
-    
-    <img src="https://zerodha.com/static/images/landing.svg" height={400} weight={400}  alt=""/>
-    <h3>Invest in everything</h3>
+    <section style={{textAlign:"center"}}  >
+   <Header/>
+    <img src="https://zerodha.com/static/images/landing.svg" alt="" />
+    <h2>Invest in everything</h2>
     <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-    <button id="btn">Signup for free</button>
+    <button id='btn' >SignUp</button>
+    </section>
+
+    <section>
+      <h1>cards</h1>
+    
+    <Products/>
     </section>
     </>
   )
